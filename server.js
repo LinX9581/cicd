be86461fdb9da6466e38a0d48c7c3f45d71f7c00
linx9581 <linx9581@gmail.com>
2020-08-18 23:31:46 +0800

    zcxv

--- server.js
+++ server.js
@@ -1,16 +1,9 @@
-    // importing express framework
-    const express = require("express");
-
-    const app = express();
-
-    // Respond with "hello world" for requests that hit our root "/"
-    app.get("/", function(req, res) {
-        return res.send("Hello World");
-    });
-
-    // listen to port 7000 by default
-    app.listen(process.env.PORT || 3000, () => {
-        console.log("Server is running");
-    });
-
-    module.exports = app;
+const express = require("express");
+const app = express();
+app.get("/", function(req, res) {
+    return res.send("Hello World");
+});
+app.listen(process.env.PORT || 3000, () => {
+    console.log("Server is running");
+});
+module.exports = app;
--- test.js
+++ test.js
@@ -1,6 +1,5 @@
 const request = require("supertest");
 const app = require("./server");
-
 describe("GET /", () => {
     it("respond with Hello World", (done) => {
         request(app).get("/").expect("Hello World", done);
